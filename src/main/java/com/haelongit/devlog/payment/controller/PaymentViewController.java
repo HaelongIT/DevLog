d402bd1194568012e7d99a7a01d3c06f2153eb78
SonYJ <soja0529@gmail.com>
2025-09-05 17:51:36 +0900

    feat: 결제 내역 페이지 구현

--- src/main/java/com/haelongit/devlog/payment/controller/PaymentViewController.java
+++ src/main/java/com/haelongit/devlog/payment/controller/PaymentViewController.java
@@ -1,15 +1,42 @@
 package com.haelongit.devlog.payment.controller;
 
+import com.haelongit.devlog.payment.entity.Payment;
+import com.haelongit.devlog.payment.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/payment")
 public class PaymentViewController {
 
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentViewController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    // 결제 페이지
     @GetMapping
     public String Payment() {
         return "payment";
+    }
+
+    // 결제 내역 페이지
+    @GetMapping("/my-order")
+    public ModelAndView myOrder() {
+        // PaymentService를 통해 데이터를 가져옴
+        List<Payment> paymentList = paymentService.getAllPayments();  // 결제 데이터를 가져오는 서비스 호출
+
+        // ModelAndView를 사용하여 데이터와 뷰를 함께 반환
+        ModelAndView mav = new ModelAndView("myorder");
+        mav.addObject("paymentList", paymentList);  // 가져온 데이터를 "paymentList"로 뷰에 전달
+
+        return mav;
     }
 }
